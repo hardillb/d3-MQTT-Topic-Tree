2d387673708690496bb4f0d40ed050ad0d62863d
timoline <timoline@gmail.com>
2015-05-17 14:12:05 +0200

    Move mqtt config to config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+
+host = '192.168.1.22';	// hostname or IP address
+//host = "broker.mqttdashboard.com";
+//port = 8000
+port = 9001;
+topic = '$SYS/#';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+// username = "jjolie";
+// password = "aa";
+cleansession = true;
